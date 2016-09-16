b13ef42524f015bbca7f4ea18453241c81d18459
Monadios <gfalkenthros@gmail.com>
2016-09-16 10:22:42 +0200

    Code linted and cleaned with Rubocop

--- bank.rb
+++ bank.rb
@@ -10,21 +10,21 @@ class Player
   def player_turn
     temp_sum = 0
     dice = 0
-    while true
+    loop do
       input = gets.chomp
       
       if want_to_move input
         dice = roll_dice
         puts "Du rullede #{dice}"
 
-        if dice > 1 and dice < 7
+        if dice > 1 && dice < 7
           temp_sum += dice
           puts "Midlertidige sum: #{temp_sum}"
         else
-          puts "Midlertidige sum nulstillet"
+          puts 'Midlertidige sum nulstillet'
           temp_sum = 0
           break
-        end        
+        end
       else
         @bank += temp_sum
         break
@@ -34,14 +34,14 @@ class Player
   end
 
   def roll_dice
-    return 1 + rand(6)
+    1 + rand(6)
   end
 
   private :roll_dice
   
   def want_to_move(input)
-    valid_pos = ["yes", "y", "ja"] #all the "yes" values
-    return valid_pos.include? input
+    valid_pos = %w(yes y ja) # all the "yes" values
+    valid_pos.include? input
   end
 end
 
@@ -54,23 +54,23 @@ class AI
     @iq = iq
   end
 
-  def player_turn(print=true)
+  def player_turn
     temp_sum = 0
     dice = 0
     avg_dice = 0
     turn_number = 1
-    while true
+    loop do
       if want_to_move(temp_sum)
         dice = roll_dice
         puts "Computeren \"#{@name}\" rullede #{dice}"
 
-        if dice > 1 and dice < 7
+        if dice > 1 && dice < 7
           temp_sum += dice
           avg_dice = (avg_dice + dice) / turn_number
           turn_number += 1
           puts "Midlertidige sum: #{temp_sum}"
         else
-          puts "Midlertidige sum nulstillet"
+          puts 'Midlertidige sum nulstillet'
           temp_sum = 0
           break
         end        
@@ -83,14 +83,13 @@ class AI
   end
 
   def roll_dice
-    return 1 + rand(6)
+    1 + rand(6)
   end
 
   private :roll_dice
 
   def want_to_move(temp_sum)
-    should_stop = 0
-    if iq == :high then should_stop = case temp_sum #the chance of stopping the turn
+    if iq == :high then should_stop = case temp_sum # the chance of stopping
                                       when (0..5) then 6
                                       when (5..10) then 4
                                       when (10..20) then 3
@@ -99,11 +98,11 @@ class AI
     elsif iq == :low
       should_stop = 3
     end
-    return rand(should_stop) != 0
+    rand(should_stop).nonzero?
   end
 
   protected :want_to_move
-  
+
 end
 
 class Board
@@ -119,17 +118,17 @@ class Board
     puts @cur_player.name
   end
   
-  def switch_player()
+  def switch_player
     temp = @players.pop # remove the current player
-    @players.unshift temp #prepend player to list
+    @players.unshift temp # prepend player to list
     @cur_player = @players[0]
   end
   
   def play
-    while true
+    loop do
       show_cur_player
       @cur_player.player_turn
-      if(@cur_player.bank >= 100)
+      if @cur_player.bank >= 100
         puts "#{@cur_player.name} vandt"
         Kernel.exit(0)
       else
@@ -146,35 +145,37 @@ class Menu
     @options = options
   end
 
-  def show_options()
-    @options.each_pair do |k,v|
+  def show_options
+    @options.each_pair do |k, v|
       puts "#{k}: #{v}"
     end
   end
 
   def create_option(str)
-    return str.downcase.gsub(/[^a-z0-9\s]/i, "").gsub(/\s+/, "_").to_sym
+    str.downcase.gsub(/[^a-z0-9\s]/i, '').gsub(/\s+/, '_').to_sym
   end
 
   
   def select_options(sym)
     players = []
-    return :not_found unless @options.has_key? sym
+    return :not_found unless @options.key? sym
     mode = (@options.assoc sym)[0]
 
+    # Replace manual typing with fields
+    
     case mode
     when :aivai
-      players[0] = AI.new("HAL", :high)
-      players[0] = AI.new("Marvin", :low)
+      players[0] = AI.new('HAL', :high)
+      players[0] = AI.new('Marvin', :low)
     when :pvp
-      puts "Spiller 1 navn?"
+      puts 'Spiller 1 navn?'
       players[0] = Player.new(gets.chomp)
-      puts "Spiller 2 navn?"
+      puts 'Spiller 2 navn?'
       players[1] = Player.new(gets.chomp)
     when :pvai
-      puts "Spiller navn?"
+      puts 'Spiller navn?'
       players[0] = Player.new(gets.chomp)
-      players[1] = AI.new("HAL", :high)
+      players[1] = AI.new('HAL', :high)
     when :exit
       Kernel.exit(0)
     end
@@ -187,8 +188,7 @@ class Menu
   end
 
   def start_menu
-    input = ""
-    while true
+    loop do
       show_options
       input = create_option gets.chomp
       selection = select_options(input)
@@ -199,12 +199,12 @@ end
 
 
 def main
-  options =  {
-              :aivai => "AI vs AI",
-              :pvp   => "Player vs Player",
-              :pvai  => "Player vs AI",
-              :exit => "Exit"
-             }
+  options = {
+             aivai: 'AI vs AI',
+             pvp:   'Player vs Player',
+             pvai:  'Player vs AI',
+             exit:  'Exit'
+            }
    
   menu = Menu.new(options)
   board = Board.new(menu)
